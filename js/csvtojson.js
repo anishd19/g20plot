34731f0671f54ac822922d7a8c8ef71492fe2d4c
Anish Dhamodaran <anishd19@gmail.com>
2016-06-21 17:24:40 +0530

    changes parser to select columns automatically

--- js/csvtojson.js
+++ js/csvtojson.js
@@ -5,31 +5,51 @@ const fs = require ("fs");
 
 const key = ["country", "population", "gdp", "purchasing_power"];
 
-const cols = [0, 5, 9, 17];
-
-var parser = function (inFile, key, cols, outFile){
+var parser = function (inFile, key, outFile){
 	
 	var buffer = "[";
 	
 	var line = fs.readFileSync(inFile).toString().split("\n");
 	
-	for(var i=1, l=line.length-2; i<l; i++){
+	var col = [];
+	
+	for(var i=0, l=line.length-2; i<l; i++){
 		
 		var value = line[i].split(",");
 		
-		cols.forEach(function(val, index, arr){
+		if(i==0){
+			value.forEach(function(val, index, arr){
+				switch(true){
+					case (val.indexOf("Country Name") > -1):
+						col[0] = index;
+						break;
+					case (val.indexOf("Population (Millions) - 2013") > -1):
+						col[1] = index;
+						break;
+					case (val.indexOf("GDP Billions (US$) - 2013") > -1):
+						col[2] = index;
+						break;
+					case (val.indexOf("Purchasing Power in Billions ( Current International Dollar) - 2013") > -1):
+						col[3] = index;
+						break;
+				}
+			});
+			
+		}else{
+			col.forEach(function(val, index, arr){
 		
-			if(index == 0)
-				buffer += "{";
-			
-			buffer += '"' + key[index] + '"' + ":" + value[val];
-			
-			if(!(index == arr.length - 1))
-				buffer += ",";
-			
-			if(index == arr.length - 1)
-				buffer += "}";
-		});
+				if(index == 0)
+					buffer += "{";
+				
+				buffer += '"' + key[index] + '"' + ":" + value[val];
+				
+				if(!(index == arr.length - 1))
+					buffer += ",";
+				
+				if(index == arr.length - 1)
+					buffer += "}";
+			});
+		}
 		
 		if(i != l-1)
 			buffer += ",";
@@ -43,4 +63,4 @@ var parser = function (inFile, key, cols, outFile){
 	console.log("parsed json saved to: " + outFile);
 }
 
-parser(inputFile, key, cols, outputFile);
+parser(inputFile, key, outputFile);
